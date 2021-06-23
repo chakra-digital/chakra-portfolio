73659ef07aa87ab76703a864b4e41f6e96a6db99
chakradojo <info@chakradojo.com>
2021-06-23 11:41:24 -0500

    moving onto proj section

---
+++ src/components/Projects/ProjectThumbnail.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+const ProjectThumbnail = () => {
+  return (
+    <section className="project-thumbnail">
+      {/* imgObj
+      <em>title</em>
+      category of proj url */}
+    </section>
+  )
+}
+
+export default ProjectThumbnail
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import HomepageHero from "../components/HomepageHero/HomepageHero"
+import ProjectThumbnail from "../components/Projects/ProjectThumbnail"
 
 import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
@@ -12,8 +13,17 @@ const IndexPage = () => {
     <Layout>
       <Seo title="Home" />
       <HomepageHero />
+      {/* <div>the box with all the projthumbnails
+        projectthumbnails
+        projectthumbnails (uniqe id for that proj)
+        projectthumbnails
+        projectthumbnails
+        projectthumbnails
+      </div> */}
     </Layout>
   )
 }
 
 export default IndexPage
+
+// Presentational vs layout components 💡
